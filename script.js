5d0eb8052f74216586bd104079e92bf3bb5e1da1
Thanawat Gulati <thanawatgulatijames@gmail.com>
2020-06-25 00:12:28 +0700

    fate3 - script and index pages

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-const imageUpload = document.getElementById("imageUpload");
+const imageUpload = document.getElementById("imageUpload")
 
-faceapi.nets.faceRecognitionNet.loadFromUri("/models");
-// faceapi.nets.faceRecognition;
-// Net
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models")
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models")
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models")
+
+])
+
+async function start(){
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const LabeledFaceDescriotors = await loadLabeledImages()
+  const faceMatcher = new faceapi.faceMatcher(LabeledFaceDescriotors,0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change',async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    const image = await faceapi.bufferToImage(imageUpload.file[0])
+    container.append(image)
+    const canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image)
+    .withFaceLandmarks().withFaceDescriptors()
+    const resizedDetection = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetection.map(d => faceMatcher.findBastMatch(d.descriptor))
+    resizedDetection.forEach(detections => {
+      const box = resizedDetectionp[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box,{label:results.toString() })
+      drawBox.draw(canvas)
+    })
+  })
+
+}
+
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America' , 'cat']
+  return Promise.all(
+    labels.map(async label => {
+      for(let i = 1;i<=2;i++){
+        const img = await faceapi.fetchImage('https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg')
+        const detections = await faceapi.detectSingleFace(img)
+        .withFaceLandmarks().withFaceDescriptors()
+        descritions.push(detections.descriptor)
+      }
+      return new faceapi.LabeledFaceDescriotors(label, descritions)
+    })
+  )
+}
--- src/style.js
+++ src/style.js
@@ -1,8 +1,34 @@
 (() => {
-  const y = Math.floor(Math.random() * 100);
   var elementStyle = document.getElementById("click").style;
   elementStyle.position = "absolute";
+  var catposi = document.getElementById("cat").style;
+  catposi.position = "absolute";
+  fetch(
+    "https://api.giphy.com/v1/gifs/random?api_key=iwHjZ72SkauAnjI3EHgbu78QoFIFMo1R&tag=cat&rating=G&limit=1"
+  )
+    .then(r => r.json())
+    .then(json => json.data.image_url)
+    .then(imageUrl => {
+      document.getElementById("cat").src = imageUrl;
+    });
+  setInterval(() => {
+    var y = Math.floor(Math.random() * 100);
+    var x = Math.floor(Math.random() * 100);
+    var xcat = Math.floor(Math.random() * 100);
+    var ycat = Math.floor(Math.random() * 100);
+    catposi.left = `${ycat}%`;
+    catposi.top = `${xcat}%`;
+    elementStyle.left = `${y}%`;
+    elementStyle.top = `${x}%`;
+  }, 800);
 
-  elementStyle.left = `${y}%`;
-  elementStyle.top = `${y}%`;
+  setInterval(() => {
+    document.getElementById("fy").style.color =
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+    document.getElementById("fy").style.fontSize = `${Math.floor(
+      Math.random() * 100
+    )}pt`;
+    document.body.style.backgroundColor =
+      "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+  }, 500);
 })();
