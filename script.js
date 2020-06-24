0753e5bb3ba749646b88b1b1c33b22e1fb40afff
Thanawat Gulati <thanawatgulatijames@gmail.com>
2020-06-25 00:19:30 +0700

    script

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri("/models")
   faceapi.nets.faceLandmark68Net.loadFromUri("/models")
   faceapi.nets.ssdMobilenetv1.loadFromUri("/models")
-
 ])
 
 async function start(){
