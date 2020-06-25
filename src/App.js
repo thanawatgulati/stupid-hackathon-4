81d1682447c195c79d0e38f4a02ab37046b02b8b
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-25 23:38:50 +0700

    🦜 Design Main and Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/login";
+import Main from "./components/main";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Route path="/login" component={Login} />
+        <Route path="/main" component={Main} />
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/ticket.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Ticket() {
+  return (
+    <div className="grid grid-flow-col bg-blue-400 rounded-lg text-white mt-4">
+      <div>
+        <p className="text-2xl pt-6 px-3 mb-0">สาย</p>
+        <p className="text-5xl px-3 mt-auto">203</p>
+      </div>
+      <div>
+        <p className="text-4xl pt-1 px-3 mb-0 text-right">25/06/63</p>
+        <p className="text-2xl px-3 mb-0  text-right">หมายเลขบัตร : 00001</p>
+        <p className="text-2xl px-3 mb-0  text-right">เวลา : 00.01 น.</p>
+      </div>
+    </div>
+  );
+}
