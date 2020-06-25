214186db57b10f55aa84d6f430874f70776c1184
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-25 23:46:13 +0700

    Co-authored-by: Kittidet Cheavachirakul <fvgbhv14@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/login";
 import Main from "./components/main";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+      <Switch>
         <Route path="/login" component={Login} />
         <Route path="/main" component={Main} />
-      </div>
+        <Route path="*" component={Login} />
+      </Switch>
     </Router>
   );
 }
