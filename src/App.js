319372e152d678bb859492e8e9f71dd379a30913
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-26 14:53:11 +0700

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/login";
 import Main from "./components/main";
 import Pay from "./components/pay";
 import Topup from "./components/topup";
+import firebase from "firebase";
 
-export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/main" component={Main} />
-        <Route exact path="/pay" component={Pay} />
-        <Route exact path="/topup" component={Topup} />
-        <Route exact path="*" component={Login} />
-      </Switch>
-    </Router>
-  );
+// const SecuredRoute = ({ component: Component, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) =>
+//       isLoggedIn() === true ? (
+//         <Component {...props} />
+//       ) : (
+//         <Redirect to="/login" />
+//       )
+//     }
+//   />
+// );
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtUoO5drwqnPKTN8DxOhz7IWumDyOSnfE",
+  authDomain: "meypay-sht4.firebaseapp.com",
+});
+
+export default class App extends Component {
+  state = { isSignIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.FacebookAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignIn: !!user });
+      console.log(user);
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Login uiConfig={this.uiConfig} />
+            </Route>
+            <Route exact path="/main" component={Main} />
+            <Route exact path="/pay" component={Pay} />
+            <Route exact path="/topup" component={Topup} />
+            <Route exact path="*" component={Login} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "../assets/login.css";
 import { Link } from "react-router-dom";
-export default function Login() {
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+
+export default function Login({ uiConfig }) {
   return (
     <div className="body-login">
       <div className="text-center text-white mt-40">
         <p className="text-6xl">MEYPAY</p>
         <p>ให้การขึ้นรถเมล์ของคุณเป็นเรื่องยาก</p>
-        <Link to="/main">
-          <button className="mt-24 text-gray-900 bg-white p-4 rounded-full shadow-lg hover:bg-blue-700 hover:text-white">
-            เข้าสู่ระบบด้วย Facebook
-          </button>
-        </Link>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     </div>
   );
